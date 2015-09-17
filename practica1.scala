b4b425210626e7fb792a16f5d3ae7ddb613a6d3e
jafetismael <jack_sanchez10@hotmail.com>
2015-09-16 19:38:38 -0500

    termino ejercicio1

---
+++ practica1.scala
@@ -0,0 +1,51 @@
+EJERCICIO 1 
+a)
+
+println (Primera final)
+val calificacion1 = read float()
+println (Segunda calificaion)
+val calificacion2 = read float()
+println (Tercera calificaion)
+val calificacion3 = read float()
+println (Examen final)
+val calificaionExamenfinal =read float()
+println (Trabajo final)
+val calificaionTrabajofinal =read float()
+val primedio =(calificacion1+calificacion2+calificacion3)3
+val final = promedio  55 + calificacionExamenfinal3 + Trabajofinal1.5
+println(calificacion = + final)
+
+
+b)
+
+println (cantidad 1)
+val cantidad1 = read float()
+println (cantidad 2)
+val cantidad2 = read float()
+println (cantidad 3)
+val cantidad3 = read float()
+val total = cantidad1 + cantidad2 + cantidad3
+println (procentaje1)
+val porcentaje1= cantidad1 + 100 total
+println (porcentaje1)
+val porcentaje2= cantidad2 + 100 total
+println (porcentaje2)
+val porcentaje3= cantidad3 + 100 total
+println (porcentaje3)
+
+c)
+
+println (presupesto 1)
+val presupuesto1 = read float()
+println (presupesto 2)
+val presupuesto2= read float()
+println (presupesto 3)
+val presupuesto3 = read float()
+val total = presupesto1 + presupesto2 + presupesto3
+println (procentajepresupuestal1)
+val porcentajepresupuestal1 = cantidad1  100 3
+println (porcentaje1)
+val porcentajepresupuestal2 = cantidad3  100 3
+println (porcentaje2)
+val porcentajepresupuestal3 = cantidad3  100 3
+println (porcentaje3)
